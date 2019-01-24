931d347bc5dc4ab830e13668c2af11b7b8a6e1fb
carolynrehm <carolynhrehm@gmail.com>
2019-01-24 16:20:32 -0500

    add student demo

--- WeekB/DomDemo.js
+++ WeekB/DomDemo.js
@@ -1,15 +1,16 @@
 console.log("js success!")
 
-document.getElementById("catImage").src = "https://cdn.images.express.co.uk/img/dynamic/128/590x/lynx-909438.jpg";
+let catPic = document.getElementById("catImage");
+catPic.src = "https://cdn.images.express.co.uk/img/dynamic/128/590x/lynx-909438.jpg";
 
 let capts = document.getElementsByTagName("figcaption");
 capts[0].innerHTML = "no longer a small cat";
 
-let newNode = document.createElement("h3");
-newNode.innerHTML = "I just made this new node!";
+// let newNode = document.createElement("h3");
+// newNode.innerHTML = "I just made this new node!";
 
-let bodyNode = document.getElementsByTagName("body")[0];
-bodyNode.appendChild(newNode);
+// let bodyNode = document.getElementsByTagName("body")[0];
+// bodyNode.appendChild(newNode);
 
 let navNode = document.getElementById("nav");
 //bodyNode.removeChild(navNode);
@@ -24,4 +25,34 @@ for(i=anchors.length-1;i>-1;i--){
 
 document.getElementById("rev").click();
 
+catPic.addEventListener("mouseover", changePic);
+// [nav element].addEventListener("mouseover", navFunction);
 
+function changePic(event){
+    console.log(event);
+    // console.log(event.clientX+", "+event.clientY);
+    if(event.target.src === "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cat-quotes-1543599392.jpg?crop=1.00xw:0.759xh;0,0&resize=480:*"){
+        catPic.src = "https://cdn.images.express.co.uk/img/dynamic/128/590x/lynx-909438.jpg"
+        capts[0].innerHTML = "no longer a small cat"
+    } else{
+        catPic.src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cat-quotes-1543599392.jpg?crop=1.00xw:0.759xh;0,0&resize=480:*"; 
+        capts[0].innerHTML = "Our small cat"
+    }
+}
+
+catPic.addEventListener("mouseover", displayText);
+catPic.addEventListener("mouseleave", removeText);
+
+function displayText(){
+    let newNode = document.createElement("h3");
+    newNode.innerHTML = "You hovered over the cat";
+    newNode.id = "hoverText"
+    
+    let bodyNode = document.getElementsByTagName("body")[0];
+    bodyNode.appendChild(newNode);
+}
+
+function removeText(){
+    let variableText = document.getElementById("hoverText");
+    variableText.remove();
+}
