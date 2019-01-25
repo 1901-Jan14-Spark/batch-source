f73cd1a6e3a0105428641649dd76ab08c792b174
Javier Rodriguez <August112641@gmail.com>
2019-01-25 15:49:09 -0500

    Removed Comments

--- JavaScriptExercise/JavaScriptExercise2.js
+++ JavaScriptExercise/JavaScriptExercise2.js
@@ -1,133 +1,134 @@
 // // 0
-// function printNumbers(){
-//     for(i=1; i<101; i++){
-//  if(i%3==0 && i%5==0)
-//     console.log("FizzBuzz");
-// else if(i%3==0)
-//     console.log("Fizz");
-// else if(i%5==0)
-//     console.log("Buzz");
-// else
-// console.log(i);
-//     }
-// }
-// // 1
-// var quack=[5, "dog", 17, "cat", "lizard Dragons", 64];
-// function maxLenght(quack){
-//     var a = quack.length;
-//     var indexLength=0;
-//     var maxString=0;
-//     var indexMax=0;
-//     var currentString="";
-//     for (var i=0; i < a; i++){
-//     currentString=quack[i];
-//     indexLength=currentString.length;
+function printNumbers(){
+    for(i=1; i<101; i++){
+ if(i%3==0 && i%5==0)
+    console.log("FizzBuzz");
+else if(i%3==0)
+    console.log("Fizz");
+else if(i%5==0)
+    console.log("Buzz");
+else
+console.log(i);
+    }
+}
+// 1
+var quack=[5, "dog", 17, "cat", "lizard Dragons", 64];
+function maxLenght(quack){
+    var a = quack.length;
+    var indexLength=0;
+    var maxString=0;
+    var indexMax=0;
+    var currentString="";
+    for (var i=0; i < a; i++){
+    currentString=quack[i];
+    indexLength=currentString.length;
     
-//     if (maxString<indexLength){
-//         (maxString=indexLength);
-//         (indexMax = i);
-//     }
-//         else{
-//             continue;
-//         }
-//     }
-//     console.log(indexMax);
+    if (maxString<indexLength){
+        (maxString=indexLength);
+        (indexMax = i);
+    }
+        else{
+            continue;
+        }
+    }
+    console.log(indexMax);
 
-// }  
+}  
     
 // // 2
-// var arr= [5, "dog", 17, "cat", "lizard", 64];
-// function reverseArray(arr){
-// let reverseArr= arr.reverse();
-// console.log(reverseArr);
-// }
+var arr= [5, "dog", 17, "cat", "lizard", 64];
+function reverseArray(arr){
+let reverseArr= arr.reverse();
+console.log(reverseArr);
+}
+
 // //3
-// function vowelsNumber(array){
-// var vowel=["a","e","i","o","u"];
-// var amount=0;
-// for (var i=0; i < array.length; i++){
-// if(array[i]== "a"){
-//     (amount++);
+function vowelsNumber(array){
+var vowel=["a","e","i","o","u"];
+var amount=0;
+for (var i=0; i < array.length; i++){
+if(array[i]== "a"){
+    (amount++);
 
-// }
-// if(array[i]== "e"){
-//     (amount++);
+}
+if(array[i]== "e"){
+    (amount++);
 
-// }
-// if(array[i]== "i"){
-//     (amount++);
+}
+if(array[i]== "i"){
+    (amount++);
 
-// }
-// if(array[i]== "o"){
-//     (amount++);
+}
+if(array[i]== "o"){
+    (amount++);
 
-// }
-// if(array[i]== "u"){
-//     (amount++);
-// }
+}
+if(array[i]== "u"){
+    (amount++);
+}
     
-// }
-// return amount;
+}
+return amount;
 
-// }
+}
 
 // // // 4  
-// function scriptRemoval(string){
-// var newString= string.replace(/Script/gi,"");
-// return newString;
-// }
+function scriptRemoval(string){
+var newString= string.replace(/Script/gi,"");
+return newString;
+}
 
 // // 5
-// function leapYear(year){
-//     if((year%4==0)&&(year%100!==0)||(year%400==0))
+function leapYear(year){
+    if((year%4==0)&&(year%100!==0)||(year%400==0))
 
-//     return "Year IS a Leap Year!";
+    return "Year IS a Leap Year!";
 
-// else
-//     return "NOT a Leap Year!"
+else
+    return "NOT a Leap Year!"
 
-// }
+}
 
 // 6
-// function validEmail(email){
-// var test=(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/);
-// return test.test(email);
-// }
+function validEmail(email){
+var test=(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/);
+return test.test(email);
+}
 
 // 7
-// function dissectChar(string,char){
+function dissectChar(string,char){
     
-//     var cut=string.slice(0,char)+string.slice(char+1,string.length);
-//     return cut;
-// }
+    var cut=string.slice(0,char)+string.slice(char+1,string.length);
+    return cut;
+}
 
 // 8
-// function bubbleSort(numArray){
-//     var size = numArray.length;
-//     var swapped;
-//     do {
-//         swapped = false;
-//         for (var i = 0; i < size; i++) {
-//             if (numArray[i] > numArray[i + 1]) {
-//                 var temp = numArray[i];
-//                 numArray[i] = numArray[i + 1];
-//                 numArray[i + 1] = temp;
-//                 swapped = true;
-//             }
-//         }
-//     } while (swapped);
-//     return numArray;
-// }
+function bubbleSort(numArray){
+    var size = numArray.length;
+    var swapped;
+    do {
+        swapped = false;
+        for (var i = 0; i < size; i++) {
+            if (numArray[i] > numArray[i + 1]) {
+                var temp = numArray[i];
+                numArray[i] = numArray[i + 1];
+                numArray[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return numArray;
+}
 
 // // 9
-// function isPalindrome(string){
-// if (string == string.split('').reverse().join('')) {
-//    return true;
-// }
-// else {
-//     return false;
-// }
-// }
+function isPalindrome(string){
+if (string == string.split('').reverse().join('')) {
+   return true;
+}
+else {
+    return false;
+}
+}
 
 // 10//Seperated Diamond not working fully
 function square(){
