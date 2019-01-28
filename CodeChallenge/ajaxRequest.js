c3198af5df41c5615173e634361dbadbdd93c82f
Javier Rodriguez <August112641@gmail.com>
2019-01-28 10:30:41 -0500

    Attempt at Code Challenge

---
+++ CodeChallenge/ajaxRequest.js
@@ -0,0 +1,15 @@
+http://testapp-env.hnpsxrq3bj.us-east-1.elasticbeanstalk.com:8080/message
+// 0 set up the new request
+// 1 checks that the server is working status "200",
+// 2 provides address && Sends
+
+// 3 headers recieved
+// 4 Loading
+function sen(){
+   new XMLHttpRequest()
+     onreadystatechange = "200";
+let send =('Get', 'http://testapp-env.hnpsxrq3bj.us-east-1.elasticbeanstalk.com:8080/message');
+send;
+}
+let para=document.getElementsByTagName("p");
+para.innerHTML=sen;
