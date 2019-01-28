13d17d0d68211086fc0dad5f9c0cf2ba7116ba8d
Agilay Dones <agilaydones@gmail.com>
2019-01-28 10:47:46 -0500

    codeChalllange.js

---
+++ codeChalllange.js
@@ -0,0 +1,14 @@
+
+let baseUrl =  "http://testapp-env.hnpsxrq3bj.us-east-1.elasticbeanstalk.com:8080/";
+text = JSON.parse.baseUrl;
+console.log(text)
+
+window.onload = function(){
+    getInfo() }
+
+
+function getInfo(baseUrl,message){
+var display = `${message} from ${baseUrl}`;
+console.log(display);
+}
+
