fe068c4b56886d4130aacdc75fee152968924f3e
Habtamuegigu <46689508+Habtamuegigu@users.noreply.github.com>
2019-01-28 10:47:18 -0500

    Add files via upload

---
+++ code_challange/code_challange.js
@@ -0,0 +1,9 @@
+
+    
+var code_challange= new XMLHttpRequest();
+var url="http://testapp-env.hnpsxrq3bj.us-east-1.elasticbeanstalk.com:8080/message"
+
+;          
+ console.log.code_challange;    
+     
+
