bda1717911e6b720482542aea6440d89764da40c
David Van Alstine <vanalstinedj@gmail.com>
2019-01-28 19:25:15 -0500

    remove 9 files

--- code challenge/assesment.js
+++
@@ -1,7 +0,0 @@
-// the url where all of our messages will come from 
-let baseURL = "http://testapp-env.hnpsxrq3bj.us-east-1.elasticbeanstalk.com:8080/message";
-
-// Rather than make up stuff, I'm just going to admit that I do not know how to di this assignment, and that's my fault.  I didn't prepare or spend enough time over the weekend learning and understanding AJAX and I will discuss this with you.  
-
-
-
