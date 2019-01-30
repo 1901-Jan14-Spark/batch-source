77df96289754208f80dda1784396560e245d6503
carolynrehm <carolynhrehm@gmail.com>
2019-01-30 14:10:32 -0500

    Pizza additions

--- WeekC/JavaIntro/Pizza.java
+++ WeekC/JavaIntro/Pizza.java
@@ -1,11 +1,11 @@
 public class Pizza{
 
-    String crustType;
-    String sauceType;
-    boolean hasCheese;
-    int size;
+    private String crustType;
+    private String sauceType;
+    private boolean hasCheese;
+    private int size;
 
-    static int pizzasMade = 0;
+    private static int pizzasMade = 0;
 
     Pizza(){
         crustType = "thin";
@@ -28,19 +28,69 @@ public class Pizza{
         System.out.println(p.toString());
         Pizza p2 = new Pizza();
         System.out.println(p2.toString());
-        System.out.println(pizzasMade);
-        Pizza.resetCounter();
-        System.out.println(pizzasMade);
+        // System.out.println(pizzasMade);
+        // Pizza.resetCounter();
+        // System.out.println(pizzasMade);
+        p2.setCrustType("thick");
+        p2.setSize(18);
+        System.out.println(p2.toString());
+        p2.setCrustType("cheese stuffed");
+        System.out.println(p2.toString());
+
     }
 
     // converting our object to a readable string, describing the states of that object
     // this is an instance method, it will apply to each object specifically
     public String toString(){
-        return "My Pizza: crustType="+crustType+", hasCheese="+hasCheese+", sauceType="+sauceType;
+        return "My Pizza: crustType="+crustType+", hasCheese="+hasCheese+", sauceType="+sauceType+", size="+size;
     }
 
-    static void resetCounter(){
+    private static void resetCounter(){
         pizzasMade = 0;
+    }
+
+    public static int getPizzasMade(){
+        return pizzasMade;
+    }
+
+    public String getCrustType(){
+        return crustType;
+    }
+
+    public void setCrustType(String crustType){
+        if("cheese stuffed".equals(crustType)||"thin".equals(crustType)){
+            this.crustType = crustType;
+        } else {
+            System.out.println("Invalid crust");
+        }
+    }
+
+    public String getSauceType(){
+        return sauceType;
+    }
+
+    public void setSauceType(String sauceType){
+        this.sauceType = sauceType;
+    }
+
+    public boolean getHasCheese(){
+        return hasCheese;
+    }
+
+    public void setHasCheese(boolean hasCheese){
+        this.hasCheese = hasCheese;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public void setSize(int size){
+        if(size>6 && size<25){
+            this.size = size;
+        } else {
+            System.out.println("invalid size");
+        }
     }
 
 
