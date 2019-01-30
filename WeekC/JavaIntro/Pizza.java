78be0e12de26dc091280ad4acd8b3c861558dfe1
carolynrehm <carolynhrehm@gmail.com>
2019-01-30 15:19:12 -0500

    adding toppings array

--- WeekC/JavaIntro/Pizza.java
+++ WeekC/JavaIntro/Pizza.java
@@ -4,6 +4,7 @@ public class Pizza{
     private String sauceType;
     private boolean hasCheese;
     private int size;
+    private String[] toppings = new String[3];
 
     private static int pizzasMade = 0;
 
@@ -21,6 +22,15 @@ public class Pizza{
         pizzasMade++;
     }
 
+    Pizza(String sauceType, boolean hasCheese, String[] toppings){
+        this.sauceType = sauceType;
+        this.hasCheese = hasCheese;
+        if(toppings.length==3){
+            this.toppings = toppings;
+        }
+        pizzasMade++;
+    }
+
     // including our main method 
     public static void main(String[] args){
         Pizza p = new Pizza("white",false);
@@ -31,18 +41,29 @@ public class Pizza{
         // System.out.println(pizzasMade);
         // Pizza.resetCounter();
         // System.out.println(pizzasMade);
-        p2.setCrustType("thick");
-        p2.setSize(18);
-        System.out.println(p2.toString());
-        p2.setCrustType("cheese stuffed");
-        System.out.println(p2.toString());
+        // p2.setCrustType("thick");
+        // p2.setSize(18);
+        // System.out.println(p2.toString());
+        // p2.setCrustType("cheese stuffed");
+        // System.out.println(p2.toString());
+        String[] arr = new String[3];
+
+        Pizza p3 = new Pizza("bbq",true,new String[]{"mushrooms", null, null});
+        System.out.println(p3.toString());
+        p3.addTopping("spinach");
+        p3.addTopping("onion");
+        System.out.println(p3.toString());
+        p3.removeTopping("onion");
+        System.out.println(p3.toString());
+        p3.removeTopping("tomato");
+
 
     }
 
     // converting our object to a readable string, describing the states of that object
     // this is an instance method, it will apply to each object specifically
     public String toString(){
-        return "My Pizza: crustType="+crustType+", hasCheese="+hasCheese+", sauceType="+sauceType+", size="+size;
+        return "My Pizza: crustType="+crustType+", hasCheese="+hasCheese+", sauceType="+sauceType+", size="+size+", toppings="+printToppings();
     }
 
     private static void resetCounter(){
@@ -93,5 +114,38 @@ public class Pizza{
         }
     }
 
+    public String[] getToppings(){
+        return toppings;
+    }
 
+    public void addTopping(String newTopping){
+        for(int i=0; i<toppings.length;i++){
+            if(toppings[i] == null){
+                toppings[i] = newTopping;
+                return;
+            }
+        }
+        System.out.println("too many toppings!");
+    }
+
+    public void removeTopping(String topping){
+        for(int i=0; i<toppings.length;i++){
+            if(topping.equals(toppings[i])){
+                toppings[i] = null;
+                return;
+            }
+        }
+        System.out.println("topping not found");
+
+    }
+    
+    public String printToppings(){
+        String toppingsStr = "";
+        for(int i=0;i<toppings.length;i++){
+            if(toppings[i]!=null){
+                toppingsStr+=toppings[i]+" ";
+            }
+        }
+        return toppingsStr;
+    }
 }
