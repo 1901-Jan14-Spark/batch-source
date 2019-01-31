791f85408e6a1b3b1fbc233b9649efebd0319c70
dattran91 <dattran91@hotmail.com>
2019-01-30 22:04:52 -0500

    organize my branch

--- weekB/script.js
+++
@@ -1,7 +0,0 @@
-// Define x,y, and z
-
-let x = "0" && ""; // ""  does not
-let y = 25 || null; // 25  short circuit
-let z = (0 && false) || ((5*"cat")|| 25)  // does not 25
-
-// Bonus: How would you implement a function which took a number array and return the max value?
