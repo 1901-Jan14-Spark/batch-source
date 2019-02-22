7ae0f863c6c43bf733290a88b02066ef9914a155
carolynrehm <carolynhrehm@gmail.com>
2019-02-22 11:08:30 -0500

    singleton example

---
+++ Week1/CoreDemo/src/com/revature/patterns/Driver.java
@@ -0,0 +1,17 @@
+package com.revature.patterns;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		MySingleton singleton1 = MySingleton.getInstance();
+		MySingleton singleton2 = MySingleton.getInstance();
+		System.out.println(singleton1==singleton2);
+		
+		
+		singleton1.setValue(5);
+		System.out.println(singleton2.getValue());
+
+	}
+
+}
