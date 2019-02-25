ba924897013cc5f08247f365b6ec0ad4ade8243c
David Van Alstine <vanalstinedj@gmail.com>
2019-02-25 07:49:04 -0500

    final touches on bank

--- Project0.1/src/com/revature/bank/Presentation.java
+++ Project0.1/src/com/revature/bank/Presentation.java
@@ -8,35 +8,7 @@ public class Presentation extends Service {
 	public static Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {			
-		System.out.println("__________________________");
-		System.out.println("|                        |");
-		System.out.println("|       THE BANK         |");
-		System.out.println("|________________________|");
-		System.out.println();
-		System.out.println();
-		System.out.println("1: Login\n2: Create Account");
-		int option = scan.nextInt();
-		if(option == 1) {
-			logIn();	
-		} else if (option == 2) {
-			// create a new member object
-			System.out.println("Username: ");
-			String newUsername = scan.next();
-			System.out.println("Email: ");
-			String newUserEmail = scan.next();
-			System.out.println("Password: ");
-			String newUserPassword = scan.next();
-			System.out.println("Deposit Amount: ");
-			double newUserDeposit = scan.nextDouble();
-			Member newMember = new Member(newUsername, newUserEmail, newUserPassword, newUserDeposit);
-			System.out.println("Account successfully created!");
-			addNewBankMember(newMember);
-			adjustAccountBalance(newMember.accountBalance);
-			
-		}
-		
-		
-
+		homeScreen();
 	}
 		
 	
--- Project0.1/src/com/revature/bank/Service.java
+++ Project0.1/src/com/revature/bank/Service.java
@@ -17,77 +17,143 @@ public class Service extends Persistence {
 			System.out.print("Password: ");
 			String password = sc.next();
 			if(password.equals(getAccountPassword())) {
-				// validate the user
-				while(true) {
-					// once the user has been validated, enter the transactions menu
+				transactions();	
+			} else {
+				System.out.println();
+				System.out.println("Invalid username or password!");
+				System.out.println();
+				logIn();
+			}
+		} else {
+			System.out.println();
+			System.out.println("Invalid username or password! Please re-enter: ");
+			System.out.println();
+			logIn();
+		}
+	}
+	
+	public static void homeScreen() {
+		System.out.println("__________________________");
+		System.out.println("|                        |");
+		System.out.println("|       THE BANK         |");
+		System.out.println("|________________________|");
+		System.out.println();
+		System.out.println();
+		System.out.println("1: Login\n2: Create Account");
+		int option = sc.nextInt();
+		if(option == 1) {
+			System.out.println();
+			logIn();	
+		} else if (option == 2) {
+			System.out.println();
+			System.out.println();
+			// create a new member object
+			System.out.println("Username: ");
+			String newUsername = sc.next();
+			System.out.println("Email: ");
+			String newUserEmail = sc.next();
+			System.out.println("Password: ");
+			String newUserPassword = sc.next();
+			System.out.println("Deposit Amount: ");
+			double newUserDeposit = sc.nextDouble();
+			Member newMember = new Member(newUsername, newUserEmail, newUserPassword, newUserDeposit);
+			System.out.println();
+			System.out.println();
+			System.out.println("Account successfully created!");
+			addNewBankMember(newMember);
+			adjustAccountBalance(newMember.accountBalance);	
+		}
+		
+	}
+	
+	public static void transactions() {
+		System.out.println();
+		// once the user has been validated, enter the transactions menu
+		System.out.println("My Account\n----------");
+		System.out.println("Account: " + getAccountNumber());
+		System.out.println();
+		System.out.println();
+		System.out.println("1: View Balance\n2: Make Deposit\n3: Make Withdrawal\n4: Log Out");
+		int option = sc.nextInt();
+		while (option != 4) {
+			if(option == 2) {
+				System.out.println();
+				System.out.println("Balance: $" + retrieveAccountBalance());
+				System.out.println("Deposit Amount: ");
+				double amount = sc.nextDouble();
+				makeDeposit(amount);
+				System.out.println("Would you like another transaction? (Y/N)");
+				String answer = sc.next();
+				if(answer.equals("Y")) {
+					System.out.println();
 					System.out.println("My Account\n----------");
 					System.out.println("Account: " + getAccountNumber());
 					System.out.println();
 					System.out.println();
 					System.out.println("1: View Balance\n2: Make Deposit\n3: Make Withdrawal\n4: Log Out");
-					int option = sc.nextInt();
-					while (option != 4) {
-						if(option == 2) {
-							System.out.println("Deposit Amount: ");
-							double amount = sc.nextDouble();
-							makeDeposit(amount);
-							System.out.println("Would you like another transaction? (Y/N)");
-							String answer = sc.next();
-							if(answer.equals("Y")) {
-								System.out.println("1: View Balance\n2: Make Deposit\n3: Make Withdrawal\n4: Log Out");
-								option = sc.nextInt();
-							} else if (!answer.equalsIgnoreCase("Y") || !answer.equalsIgnoreCase("N")) {
-								System.out.println("Invalid Input! Please select a valid option: (Y/N)");
-								answer = sc.next();
-							} else {
-								logOut();
-							}
-						} else if(option == 3) {
-							System.out.println("Withdrawal Amount: ");
-							double amount = sc.nextDouble();
-							makeWithdrawal(amount);
-							System.out.println("Would you like another transaction? (Y/N)");
-							String answer = sc.next();
-							if(answer.equals("Y")) {
-								System.out.println("1: View Balance\n2: Make Deposit\n3: Make Withdrawal\n4: Log Out");
-								option = sc.nextInt();
-							} else if (!answer.equalsIgnoreCase("Y") || !answer.equalsIgnoreCase("N")) {
-								System.out.println("Invalid Input! Please select a valid option: (Y/N)");
-								answer = sc.next();
-							} else {
-								logOut();
-							}
-						} else if (option == 1){
-							viewBalance();
-							System.out.println("Would you like another transaction? (Y/N)");
-							String answer = sc.next();
-							if(answer.equals("Y")) {
-								System.out.println("1: View Balance\n2: Make Deposit\n3: Make Withdrawal\n4: Log Out");
-								option = sc.nextInt();
-							} else if (!answer.equalsIgnoreCase("Y") || !answer.equalsIgnoreCase("N")) {
-								System.out.println("Invalid Input! Please select a valid option: (Y/N)");
-								answer = sc.next();
-							} else {
-								logOut();
-							}
-						}else {
-							System.out.println("Please select a valid option!");
-							System.out.println("1: View Balance\n2: Make Deposit\n3: Make Withdrawal\n4: Log Out");
-							option = sc.nextInt();						
-						}
-					}
-					if (option == 4) {
-						logOut();
-					}
+					option = sc.nextInt();
+				} else if (!answer.equalsIgnoreCase("Y") && !answer.equalsIgnoreCase("N")) {
+					System.out.println("Invalid Input! Please select a valid option: (Y/N)");
+					answer = sc.next();
+				} else if(answer.equalsIgnoreCase("N")) {
+					option = 4;
 				}
-			}		
-		}		
+			} else if(option == 3) {
+				System.out.println();
+				System.out.println("Balance: $" + retrieveAccountBalance());
+				System.out.println("Withdrawal Amount: ");
+				double amount = sc.nextDouble();
+				makeWithdrawal(amount);
+				System.out.println("Would you like another transaction? (Y/N)");
+				String answer = sc.next();
+				if(answer.equals("Y")) {
+					System.out.println();
+					System.out.println("My Account\n----------");
+					System.out.println("Account: " + getAccountNumber());
+					System.out.println();
+					System.out.println();
+					System.out.println("1: View Balance\n2: Make Deposit\n3: Make Withdrawal\n4: Log Out");
+					option = sc.nextInt();
+				} else if (!answer.equalsIgnoreCase("Y") && !answer.equalsIgnoreCase("N")) {
+					System.out.println("Invalid Input! Please select a valid option: (Y/N)");
+					answer = sc.next();
+				} else if (answer.equalsIgnoreCase("N")) {
+					option = 4;
+				}
+			} else if (option == 1){
+				viewBalance();
+				System.out.println("Would you like another transaction? (Y/N)");
+				String answer = sc.next();
+				if(answer.equals("Y")) {
+					System.out.println();
+					System.out.println("My Account\n----------");
+					System.out.println("Account: " + getAccountNumber());
+					System.out.println();
+					System.out.println();
+					System.out.println("1: View Balance\n2: Make Deposit\n3: Make Withdrawal\n4: Log Out");
+					option = sc.nextInt();
+				} else if (!answer.equalsIgnoreCase("Y") && !answer.equalsIgnoreCase("N")) {
+					System.out.println("Invalid Input! Please select a valid option: (Y/N)");
+					answer = sc.next();
+				} else if(answer.equalsIgnoreCase("N")) {
+					option = 4;
+				}
+			}else {
+				System.out.println("Please select a valid option!");
+				System.out.println("1: View Balance\n2: Make Deposit\n3: Make Withdrawal\n4: Log Out");
+				option = sc.nextInt();						
+			}
+		}
+		if (option == 4) {
+			logOut();
+		}
+		
 	}
 	
 	// log out method
 	public static void logOut() {
-		System.out.println("Have a great day!");
-		
+		System.out.println();
+		homeScreen();		
 	}
 	
 	// add money to a user's account
@@ -99,9 +165,9 @@ public class Service extends Persistence {
 		double newBalance;
 		// add the deposit amount to the current balance to create the new balance
 		newBalance = currentBalance + amount;
+		System.out.println();
 		// let the user know what how much they deposited
 		System.out.println("Deposit Amount: $" + amount);
-		System.out.println();
 		// display the new account balance
 		System.out.println("Account Balance: $" + newBalance);
 		// adjust the nbalance int the text file to reflect the new balance
