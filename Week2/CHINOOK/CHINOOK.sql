969e86596e9154e9d9879d81432dc57ed69add8b
AssayeAssefagit <brownshaun97@yandex.com>
2019-02-27 17:04:26 -0500

    A-C

--- Week2/CHINOOK/CHINOOK.sql
+++ Week2/CHINOOK/CHINOOK.sql
@@ -1,3 +1,13 @@
---1
+--a
 --select * 
 --FROM CHINOOK.EMPLOYEE;
+
+--b
+--select * 
+--FROM CHINOOK.EMPLOYEE
+--WHERE LASTNAME = 'King';
+
+--c
+select * 
+FROM CHINOOK.EMPLOYEE
+WHERE firstname = 'Andrew' AND REPORTSTO = NULL;
