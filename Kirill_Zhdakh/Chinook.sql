51c94e34d73b402a8e442695a55d45097ad07ea7
Kirill Zhdakh <kzhdakh@fullsail.edu>
2019-03-01 15:31:05 -0500

    Added chinook, changed bank proj to maven

--- Kirill_Zhdakh/Chinook.sql
+++ Kirill_Zhdakh/Chinook.sql
@@ -31,5 +31,11 @@ INSERT INTO CHINOOK.GENRE VALUES (1000, 'Gorecore');
 INSERT INTO CHINOOK.GENRE VALUES (1001, 'Nightcore');
 
 --B
+INSERT INTO CHINOOK.EMPLOYEE values (3234, 'Lossman', 'Liam', 'VP Quality Control', NULL, '04-Oct-1993', '01-Oct-2007', '07 Oakridge Lane', 'South Bend', 'Indiana', 'United States', '46620', '+1 (574) 715-8987', '+1 (513) 489-4955', 'llossman0@auda.org.au'); 
+INSERT INTO CHINOOK.EMPLOYEE values (2313, 'Fleeming', 'Hastie', 'Assistant Media Planner', NULL, '27-Jan-1995', '18-Jan-2007', '91 New Castle Terrace', 'Denver', 'Colorado', 'United States', '80249', '+1 (303) 298-5155', '+1 (253) 114-4623', 'hfleeming1@list-manage.com');
+
+--C
+INSERT INTO CHINOOK.CUSTOMER values (3234, 'Lossman', 'Liam', 'VP Quality Control', NULL, '04-Oct-1993', '01-Oct-2007', '07 Oakridge Lane', 'South Bend', 'Indiana', 'United States', '46620', '+1 (574) 715-8987', '+1 (513) 489-4955', 'llossman0@auda.org.au'); 
+INSERT INTO CHINOOK.CUSTOMER values (2313, 'Fleeming', 'Hastie', 'Assistant Media Planner', NULL, '27-Jan-1995', '18-Jan-2007', '91 New Castle Terrace', 'Denver', 'Colorado', 'United States', '80249', '+1 (303) 298-5155', '+1 (253) 114-4623', 'hfleeming1@list-manage.com');
 
 
