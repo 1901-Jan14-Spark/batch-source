51f2f1200a13c7d0dfb4ba684b6e2f356641efbf
Agilay Dones <agilaydones@gmail.com>
2019-03-05 18:16:54 -0500

    finishedNoTesting1616

--- Project0-Progress/project0-1/src/main/java/com/rev/main/Driver.java
+++ Project0-Progress/project0-1/src/main/java/com/rev/main/Driver.java
@@ -25,7 +25,7 @@ public class Driver {
 	
 	public static  void Login() {
 		int cId = 0;
-		log.info("\t\t\t\t\t\t\tAre you logining in or signing up?");
+		log.info("\t\t\t\t\t\t\tAre you loggining in or signing up?");
 		log.info("Type: Login or Sign");
 		String ans = scan1.nextLine();
 		
@@ -47,11 +47,20 @@ public class Driver {
 			log.info("Password: ");
 			String ans3 = scan1.nextLine();
 			if(cus1.getCustomerCredentials().get(ans2).equals(ans3)) {
+				for(int i=0; i<30; i++) {
+					log.info("");
+				}
 				log.info("You're Login Has Been Succesful");
 				cId=cus1.getIdByUser(ans2);
 				operations(cId);
 				return;
+			}else {
+				log.info("Sorry but that is the wrong password");
+				Login();
 			}
+		}else{
+			log.info("Sorry, but that username is not in our system");
+			Login();
 		}
 		
 	}
@@ -60,7 +69,7 @@ public class Driver {
 		CustomerDao cus1 = new CustomerDaoImpl();
 		int cId = 0;
 		log.info("\t\t\t\t\t\t\tCreate an account");
-		log.info("\t\t\t\tPlease Create a username");
+		log.info("\t\t\tPlease Create a username");
 		log.info("");
 		String name = "";
 		String username = scan1.nextLine();
@@ -114,14 +123,25 @@ public class Driver {
 	}
 	public static void operations(int id) {
 		CustomerDao cus1 = new CustomerDaoImpl();
+		for(int i=0; i<5; i++) {
+			log.info("");
+		}
 		log.info("What Operation would you Like to Preform?");
+		for(int i=0; i<5; i++) {
+			log.info("");
+		}
 		log.info("Deposit\t\t\t\tWithdraw\t\t\t\tView Balance\t\t\t\t\t\tLogout");
+		log.info("Type D\t\t\t\tType W\t\t\t\t\t  Type V\t\t\t\t\t\tType L");
+		for(int i=0; i<5; i++) {
+			log.info("");
+		}
 		String op = scan1.nextLine();
 		if(op.equals("d")||op.equals("D")||op.equals("deposit")||op.equals("Deposit")){
 			log.info("How much would you like to deposit?");
 			String dep = scan1.nextLine();
+			dep = dep.toLowerCase();
 			if(dep.matches(".*[a-z].*")) {
-				log.info("The deposite cannot contain letters");
+				log.info("We cannot deposit letters. Only money. Enter a numeric value");
 				operations(id);
 			}
 			double deposit = Double.parseDouble(dep);
@@ -146,6 +166,10 @@ public class Driver {
 			operations(id);
 		}
 		else if(op.equals("exit")||op.equals("logout")||op.equals("l")||op.equals("e")||op.equals("E")||op.equals("Logout")) {
+			
+			for(int i = 0; i<50; i++) {
+				log.info("");
+			}
 			log.info("Logout Successful. Goodbye.");
 			log.info("");log.info("");log.info("");log.info("");log.info("");log.info("");log.info("");log.info("");log.info("");log.info("");log.info("");log.info("");log.info("");log.info("");log.info("");
 			Login();
