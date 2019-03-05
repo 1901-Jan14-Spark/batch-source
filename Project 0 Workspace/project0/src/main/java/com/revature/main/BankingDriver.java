3b0dee5b6b907405799d9a596ed5c16d574206cf
djbogar <46689626+djbogar@users.noreply.github.com>
2019-03-04 19:09:59 -0500

    update_2

--- Project 0 Workspace/project0/src/main/java/com/revature/main/BankingDriver.java
+++ Project 0 Workspace/project0/src/main/java/com/revature/main/BankingDriver.java
@@ -1,5 +1,27 @@
 package com.revature.main;
 
-public class BankingDriver {
+import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+
+public class BankingDriver {
+	static Scanner scanner = new Scanner(System.in);
+	public final static Logger log = Logger.getRootLogger();
+	
+	
+	public static  void main(String[] args) {
+		Start();
+	}
+	
+	public static void Start() {
+		
+	}
+	
+	public static void Create() {
+		
+	}
+	
+	public static void Login() {
+		
+	}
 }
