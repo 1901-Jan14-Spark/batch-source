ff2b5519648048300765e2e74a939e25d51e4684
Kirill Zhdakh <kzhdakh@fullsail.edu>
2019-03-05 23:03:29 -0500

    JUnit for password strength checking

--- Kirill_Zhdakh/Kirill_Zhdakh_Bank/src/Util/JUnitTest.java
+++ Kirill_Zhdakh/Kirill_Zhdakh_Bank/src/Util/JUnitTest.java
@@ -7,8 +7,23 @@ import org.junit.Test;
 public class JUnitTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInvalidPassword() {
+		
+		assertFalse("12312SAdas", "12312SAdas".matches("^((?=\\S*?[A-Z])(?=\\S*?[a-z])(?=\\S*?[0-9])(?=\\S*?[!@#$%^&*]).{8,})\\S$"));
 	}
 
+	@Test
+	public void testInvalidPassword2() {
+		assertFalse("R0c!s", "R0c!s".matches("^((?=\\S*?[A-Z])(?=\\S*?[a-z])(?=\\S*?[0-9])(?=\\S*?[!@#$%^&*]).{8,})\\S$"));
+	}
+	
+	@Test
+	public void testValidPassword() {
+		assertTrue("R0tos#124", "R0tos#124".matches("^((?=\\S*?[A-Z])(?=\\S*?[a-z])(?=\\S*?[0-9])(?=\\S*?[!@#$%^&*]).{8,})\\S$"));
+	}
+	
+	@Test
+	public void testInvalidPassword3() {
+		assertFalse("oasdaosdado@A", "oasdaosdado@A".matches("^((?=\\S*?[A-Z])(?=\\S*?[a-z])(?=\\S*?[0-9])(?=\\S*?[!@#$%^&*]).{8,})\\S$"));
+	}
 }
