93b46fc165af7567f184d64cdbb60e8b46555022
dattran91 <dattran91@hotmail.com>
2019-03-08 19:53:49 -0500

    add re-login

--- Week3/Servlet Exercise/login/src/main/java/com/revature/servlets/LoginServlet.java
+++ Week3/Servlet Exercise/login/src/main/java/com/revature/servlets/LoginServlet.java
@@ -17,25 +17,32 @@ public class LoginServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		System.out.println("do get request handler method invoked in our login servlet");
-		
+
 		RequestDispatcher rd = request.getRequestDispatcher("Login.html");
 		rd.forward(request, response);
 	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		
+
 		Boolean result = loginService.validate(username, password);
-		
+
 		PrintWriter pw = response.getWriter();
-		pw.write("Login: " + result);
+
+		response.setContentType("text/html;charset=UTF-8");
+
+		pw.write("<p> Your result is: " + result + "</p>");
+		if (result == false) {
+			pw.write("<p><a href=\"login.html\" > Login Again! </a> </p>");
+		}
+
 		pw.close();
-		 
-		
 	}
-	
+
 }
