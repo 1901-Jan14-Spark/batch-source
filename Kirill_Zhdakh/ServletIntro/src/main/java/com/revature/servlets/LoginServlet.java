cc3aab3da00fccf92753a4ab55fc6e21f270bf1d
Kirill Zhdakh <kzhdakh@fullsail.edu>
2019-03-10 23:44:12 -0400

    Working with servlets to do a login

---
+++ Kirill_Zhdakh/ServletIntro/src/main/java/com/revature/servlets/LoginServlet.java
@@ -0,0 +1,32 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		RequestDispatcher rd = request.getRequestDispatcher("Login.html");
+		rd.forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		PrintWriter pw = response.getWriter();
+		
+		if (username.equals("admin") && password.equals("password"))
+			pw.write("<p> Login successful! </p>");
+		else
+			pw.write("<p> Invalid login credentioals. Please try again. </p>");
+	}
+}
