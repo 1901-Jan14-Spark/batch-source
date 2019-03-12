dfbd7de41f8ca063ad8e0dc85378c035843b3fcc
David Van Alstine <vanalstinedj@gmail.com>
2019-03-12 08:52:36 -0400

    project 1 stuff

---
+++ Project1/src/main/java/com/revature/company/Reimbursement.java
@@ -0,0 +1,10 @@
+package com.revature.company;
+
+public class Reimbursement {
+
+	
+	private int reimbursementId;
+	private int employeeId;
+	private boolean approved;
+	
+}
---
+++ Project1/src/main/java/com/revature/util/ConnectionUtil.java
@@ -0,0 +1,21 @@
+package com.revature.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+	
+	private static Connection connection;
+	
+	public static Connection getConnection() throws SQLException {
+	
+		String url = System.getenv("P1_URL");
+		String user = System.getenv("P1_USER");
+		String password = System.getenv("P1_PASS");
+		if (connection == null || connection.isClosed()) {
+			connection = DriverManager.getConnection(url,  user,  password);
+		}
+		return connection;
+	}
+}
