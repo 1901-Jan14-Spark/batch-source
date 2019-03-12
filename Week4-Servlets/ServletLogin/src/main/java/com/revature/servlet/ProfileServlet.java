5bf0dee0e0e11bb5081b49305757f843d3081d08
carolynrehm <carolynhrehm@gmail.com>
2019-03-12 16:05:47 -0400

    login demo before SessionServlet

---
+++ Week4-Servlets/ServletLogin/src/main/java/com/revature/servlet/ProfileServlet.java
@@ -0,0 +1,35 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProfileServlet
+ */
+public class ProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProfileServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getSession().getAttribute("username")==null) {
+			request.getSession().invalidate();
+			response.sendRedirect("login");
+		} else {
+			request.getRequestDispatcher("Profile.html").forward(request, response);
+		}
+	}
+
+}
