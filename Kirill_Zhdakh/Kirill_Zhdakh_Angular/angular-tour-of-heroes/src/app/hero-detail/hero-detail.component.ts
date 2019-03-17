85e4c4c842fdc14c48875e7dcf2fe3b54e9bdf5f
Kirill Zhdakh <kzhdakh@fullsail.edu>
2019-03-17 17:54:46 -0400

    Tour Done

--- Kirill_Zhdakh/Kirill_Zhdakh_Angular/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ Kirill_Zhdakh/Kirill_Zhdakh_Angular/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
-import {Hero} from '../classes/hero'
+import { Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+
+import {Hero} from '../classes/hero';
+import {HeroService} from '../hero.service';
+import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,10 +14,30 @@ import {Hero} from '../classes/hero'
 })
 
 export class HeroDetailComponent implements OnInit {
-  @Input() hero : Hero;
-  constructor() { }
+  hero: Hero;
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void
+  {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
--- Kirill_Zhdakh/Kirill_Zhdakh_Angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Kirill_Zhdakh/Kirill_Zhdakh_Angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../classes/hero';
 import {HeroService} from "../hero.service"
-import { from } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -11,22 +10,30 @@ import { from } from 'rxjs';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-
-  selectedHero : Hero;
   
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
-
-  onSelect(hero : Hero): void 
-  {
-    this.selectedHero = hero;
-  }
+  
   getHeroes(): void
   {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {return;}
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
