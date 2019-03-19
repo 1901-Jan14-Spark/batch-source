e0737bc449bb7ceabb66db441ca7f372f00c30f5
Agilay Dones <agilaydones@gmail.com>
2019-03-19 13:53:44 -0400

    work

--- Project_1_work/project_1_1/src/main/webapp/Scripts/ManagerPort.js
+++ Project_1_work/project_1_1/src/main/webapp/Scripts/ManagerPort.js
@@ -17,6 +17,8 @@ function popRTable(xhr){
 	console.log(requests);
 	let employeeTable = document.getElementById("view");
 	for(request of requests){
+		let aButtonId = "A" + request.employeeId+request.reason+request.amount;
+		let dButtonId = "D" + request.employeeId+request.reason+request.amount;
 		let bo = request.approved;
 		let req = "pending";
 		if (bo === true){
@@ -24,28 +26,37 @@ function popRTable(xhr){
 			else{
 				req = "Pending";
 			}
-		addRow(req, request.dateSubmitted.substr(0,10));
+		addRowP(request.employeeId,  request.dateSubmitted.substr(0,10),request.reason, request.amount, req, aButtonId, dButtonId);
 	}
 }
 
-function addRow(id, date, amount, reason, status){
+function addRowP(id, date, amount, reason, status, aBId, dBid){
 	let row = document.createElement("tr");
 	let cell1 = document.createElement("td");
 	let cell2 = document.createElement("td");
 	let cell3 = document.createElement("td");
 	let cell4 = document.createElement("td");
 	let cell5 = document.createElement("td");
+	let buttonA = document.createElement("button");
+	let buttonD = document.createElement("button");
 	row.appendChild(cell1);
 	row.appendChild(cell2);
 	row.appendChild(cell3);
 	row.appendChild(cell4);
 	row.appendChild(cell5);
+	row.appendChild(buttonA);
+	row.appendChild(buttonD);
 	cell1.innerHTML=id;
 	cell2.innerHTML=date;
 	cell3.innerHTML=amount;
 	cell4.innerHTML=reason;
 	cell5.innerHTML=status;
+	buttonA.innerHTML="Approve";
+	buttonA.name = aBId;
+	buttonD.innerHTML ="Deny";
+	buttonD.name = dBId;
 	document.getElementById("view").appendChild(row);
+	
 }
 
 //document.getElementById("view").addEventListener("click", popRTable);
