383bdcd0dbda94d2e7a44096f4c935b7ec6b6561
carolynrehm <carolynhrehm@gmail.com>
2019-03-26 11:57:08 -0400

    hibernate review

---
+++ Week6-Hibernate/BankHibernateDemo/src/main/java/com/revature/model/Account.java
@@ -0,0 +1,85 @@
+package com.revature.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Account {
+	
+	@Id
+	private int id;
+	
+	private int balance;
+	
+	private AccountType type;
+	
+//	@ManyToOne
+//	@JoinColumn(name="USER_ID")
+//	private User user;
+
+	public Account() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+//	public Account(int id, int balance, AccountType type, User user) {
+//		super();
+//		this.id = id;
+//		this.balance = balance;
+//		this.type = type;
+//		this.user = user;
+//	}
+	
+	
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public Account(int id, int balance, AccountType type) {
+	super();
+	this.id = id;
+	this.balance = balance;
+	this.type = type;
+}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public AccountType getType() {
+		return type;
+	}
+
+	public void setType(AccountType type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", balance=" + balance + ", type=" + type + "]";
+	}
+
+//	public User getUser() {
+//		return user;
+//	}
+//
+//	public void setUser(User user) {
+//		this.user = user;
+//	}
+
+	
+	
+	
+}
---
+++ Week6-Hibernate/BankHibernateDemo/src/main/java/com/revature/model/AccountType.java
@@ -0,0 +1,7 @@
+package com.revature.model;
+
+public enum AccountType {
+	
+	CHECKING, SAVINGS
+
+}
