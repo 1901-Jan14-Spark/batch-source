9eb6c8e33ce4c2afec54b65c42ab36e4a20005ff
VanalstineDJ <VanalstineDJ@gmail.com>
2019-04-07 20:48:05 -0400

    micro services exercise

---
+++ Microservices/ReimbursementService/src/main/java/com/revature/controllers/ReimbursementController.java
@@ -0,0 +1,62 @@
+package com.revature.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.models.Reimbursement;
+
+@RestController
+@RequestMapping("/reimbursements")
+public class ReimbursementController {
+
+	private Logger log = Logger.getLogger(ReimbursementController.class);
+	
+	private List<Reimbursement> reimbursements = new ArrayList<>();
+
+	public ReimbursementController() {
+		reimbursements.add(new Reimbursement(1, 1, 250.00));
+		reimbursements.add(new Reimbursement(2, 1, 250.00));
+		reimbursements.add(new Reimbursement(3, 1, 250.00));
+		reimbursements.add(new Reimbursement(4, 2, 250.00));
+		reimbursements.add(new Reimbursement(5, 2, 250.00));
+		reimbursements.add(new Reimbursement(6, 3, 250.00));
+		reimbursements.add(new Reimbursement(7, 3, 250.00));
+		reimbursements.add(new Reimbursement(8, 3, 250.00));
+		reimbursements.add(new Reimbursement(9, 3, 250.00));
+		reimbursements.add(new Reimbursement(10, 3, 250.00));
+		reimbursements.add(new Reimbursement(11, 4, 250.00));
+		reimbursements.add(new Reimbursement(12, 5, 250.00));
+		reimbursements.add(new Reimbursement(13, 5, 250.00));
+		reimbursements.add(new Reimbursement(14, 5, 250.00));
+		reimbursements.add(new Reimbursement(1, 5, 250.00));
+	}
+	
+	public List<Reimbursement> getAllReimbursements() {
+		log.info("getting all reimbursements");
+		return reimbursements;
+	}
+	
+	@GetMapping("/{reimbursementId}")
+	public Reimbursement getReimbursementById(@PathVariable("reimbursementId") int reimbursementId) {
+		log.info("getting reimbursement by id: " + reimbursementId);
+		for(Reimbursement rb: reimbursements) {
+			if(reimbursementId == rb.getReimbursementId()) {
+				return rb;
+			}
+		}
+		return null;
+	}
+	
+	@GetMapping("/employee/{employeeId}")
+	public List<Reimbursement> getReimbursementsById(@PathVariable("employeeId") Integer employeeId) {
+		return reimbursements.stream().filter(reim -> reim.getEmployeeId() == employeeId)
+				.collect(Collectors.toList());
+	}
+}
